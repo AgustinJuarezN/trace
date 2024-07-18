c8a8b56b29d2f18194f87f62967c0f239c18f387
Matuss <matiaschao22@hotmail.com>
2024-07-18 03:09:35 -0300

    update

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -44,11 +44,7 @@ export default function ContactForm() {
 				/>
 			</div>
 			<div>
-				<button
-					type="submit"
-					disabled={state.submitting}
-					className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
-				>
+				<button type="submit" disabled={state.submitting} class="btn btn--secondary w-full">
 					Enviar
 				</button>
 			</div>
