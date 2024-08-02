68fcab60b810d81438508ed0be6cd9f6ed076f7a
Matuss <matiaschao22@hotmail.com>
2024-08-02 17:50:10 -0300

    google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,21 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
 	site: 'https://trace.com.uy',
 	output: 'server',
 	adapter: vercel(),
-	integrations: [react(), mdx(), sitemap(), tailwind()],
+	integrations: [
+		react(),
+		mdx(),
+		sitemap(),
+		tailwind(),
+		partytown({
+			config: {
+				forward: ['dataLayer.push'],
+			},
+		}),
+	],
 });
